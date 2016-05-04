c118c9f50b561175f0586a9240ea8d2dc594f234
misterbykl <baykalemre@gmail.com>
2016-05-04 20:49:39 +0300

    BDayList list added

--- src/main/java/bday/BDayList.java
+++ src/main/java/bday/BDayList.java
@@ -1,9 +1,16 @@
 package bday;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.StreamHandler;
+
 /**
  * Created by misterb on 03/05/16.
  */
 public class BDayList {
+
     public static final String EMRE = "14 Temmuz";
     public static final String SEDA = "28 Eylül";
     public static final String ERSEN = "10 Ağustos";
@@ -16,4 +23,23 @@ public class BDayList {
     public static final String SUHAN = "25 Kasım";
     public static final String SINAN = "30 Haziran";
     public static final String MERT = "20 Temmuz";
+
+
+    public static Map getDGList() {
+        Map<String, String> dgListesi = new HashMap<String, String>();
+        dgListesi.put("emre", BDayList.EMRE);
+        dgListesi.put("seda", BDayList.SEDA);
+        dgListesi.put("ersen", BDayList.ERSEN);
+        dgListesi.put("basak", BDayList.BASAK);
+        dgListesi.put("murat", BDayList.MURAT);
+        dgListesi.put("senem", BDayList.SENEM);
+        dgListesi.put("isil", BDayList.ISIL);
+        dgListesi.put("caglar", BDayList.CAGLAR);
+        dgListesi.put("begum", BDayList.BEGUM);
+        dgListesi.put("suhan", BDayList.SUHAN);
+        dgListesi.put("sinan", BDayList.SINAN);
+        dgListesi.put("mert", BDayList.MERT);
+
+        return dgListesi;
+    }
 }
